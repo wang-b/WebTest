be59fbf3852caa535a49aa326aa6dd66719c12c4
Wang Bin <wangbin1334@163.com>
2015-11-12 10:48:06 +0800

    1

--- routes/product.js
+++ routes/product.js
@@ -8,13 +8,20 @@ var View = require('../renderer/view');
 
 var router = express.Router();
 
-router.use('/', function(req, res, next){
-    var view = new View('web/product/index');
+var showProduct = function (req, res, next){
+    var pid = req.params.pid || 2001;
+    var view = new View('web/product/index', {pid: pid});
     req.$view = {
         piwik: 'product',
         main: view.render()
     };
     next();
-});
+};
+
+/**
+ * 注：router.use('/', f)与router.use(f); 效果一样，都会拦截所有的子路由内的请求，router.get('/', f)则不会拦截。
+ */
+router.get('/', showProduct);
+router.use('/show/:pid', showProduct);
 
 module.exports = router;
